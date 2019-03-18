8be991e978cc8d200a7f8dfca77afb54e6b04530
hfpp2012 <hfpp2012@gmail.com>
2019-03-18 14:59:51 +0800

    05_callback

--- src/GithubApiService.ts
+++ src/GithubApiService.ts
@@ -9,12 +9,12 @@ const options = {
 };
 
 export class GithubApiService {
-  getUserInfo(userName: string) {
+  getUserInfo(userName: string, cb: (user: User) => any) {
     request.get("https://api.github.com/users/" + userName, options, (error: any, response: any, body: any) => {
       console.log(typeof body);
       // let user = new User(JSON.parse(body));
       let user: User = new User(body);
-      console.log(user);
+      cb(user);
     })
   }
 }
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,8 @@
 import { GithubApiService } from './GithubApiService';
+import { User } from './User';
 
 let svc: GithubApiService = new GithubApiService();
-svc.getUserInfo("hfpp2012");
+svc.getUserInfo("hfpp2012", (user: User) => {
+  console.log(user);
+  console.log("name: ", user.login);
+});
