a606f65a11f6bdd3b434538b6471ba6ce804f0e6
hfpp2012 <hfpp2012@gmail.com>
2019-03-18 13:54:18 +0800

    04_user_model

--- src/GithubApiService.ts
+++ src/GithubApiService.ts
@@ -1,15 +1,20 @@
 import * as request from 'request';
+import { User } from './User';
 
 const options = {
   headers: {
     'User-Agent': 'request'
-  }
+  },
+  json: true
 };
 
 export class GithubApiService {
   getUserInfo(userName: string) {
     request.get("https://api.github.com/users/" + userName, options, (error: any, response: any, body: any) => {
-      console.log(body);
+      console.log(typeof body);
+      // let user = new User(JSON.parse(body));
+      let user: User = new User(body);
+      console.log(user);
     })
   }
 }
