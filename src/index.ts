3ac814961a775717d6fe2eca81f49da37e3e2fd9
hfpp2012 <hfpp2012@gmail.com>
2019-03-19 14:42:36 +0800

    08_finished

--- src/index.ts
+++ src/index.ts
@@ -4,10 +4,15 @@ import { User } from './User';
 import { Repo } from './Repo';
 
 let svc: GithubApiService = new GithubApiService();
-svc.getUserInfo("hfpp2012", (user: User) => {
-  svc.getRepos(user.login, (repos: Repo[]) => {
-    let sortedRepos = _.sortBy(repos, [(repo: Repo) => repo.forks_count * -1])
-    user.repos = sortedRepos;
-    console.log(user);
-  })
-});
+
+if (process.argv.length < 3) {
+  console.log("必须传入用户名");
+} else {
+  svc.getUserInfo(process.argv[2], (user: User) => {
+    svc.getRepos(process.argv[2], (repos: Repo[]) => {
+      let sortedRepos = _.sortBy(repos, [(repo: Repo) => repo.forks_count * -1])
+      user.repos = sortedRepos;
+      console.log(user);
+    })
+  });
+}
