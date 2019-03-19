15ad16dc2bc65625d6c8bc1dddecf26fdf31d88a
hfpp2012 <hfpp2012@gmail.com>
2019-03-19 10:08:20 +0800

    06_repo

--- src/GithubApiService.ts
+++ src/GithubApiService.ts
@@ -1,5 +1,6 @@
 import * as request from 'request';
 import { User } from './User';
+import { Repo } from './Repo';
 
 const options = {
   headers: {
@@ -11,10 +12,17 @@ const options = {
 export class GithubApiService {
   getUserInfo(userName: string, cb: (user: User) => any) {
     request.get("https://api.github.com/users/" + userName, options, (error: any, response: any, body: any) => {
-      console.log(typeof body);
       // let user = new User(JSON.parse(body));
       let user: User = new User(body);
       cb(user);
+    })
+  }
+
+  getRepos(userName: string, cb: (repos: Repo[]) => any) {
+    request.get("https://api.github.com/users/" + userName + "/repos", options, (error: any, response: any, body: any) => {
+      // let user = new User(JSON.parse(body));
+      let repos: Repo[] = body.map((repo: any) => new Repo(repo));
+      cb(repos);
     })
   }
 }
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,12 @@
 import { GithubApiService } from './GithubApiService';
 import { User } from './User';
+import { Repo } from './Repo';
 
 let svc: GithubApiService = new GithubApiService();
 svc.getUserInfo("hfpp2012", (user: User) => {
   console.log(user);
-  console.log("name: ", user.login);
 });
+
+svc.getRepos("hfpp2012", (repos: Repo[]) => {
+  console.log(repos);
+})
