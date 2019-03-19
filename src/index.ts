e950543d15cda95e6482767df7f79b57ad67132c
hfpp2012 <hfpp2012@gmail.com>
2019-03-19 11:16:58 +0800

    07_lodash

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,13 @@
 import { GithubApiService } from './GithubApiService';
+import * as _ from 'lodash';
 import { User } from './User';
 import { Repo } from './Repo';
 
 let svc: GithubApiService = new GithubApiService();
 svc.getUserInfo("hfpp2012", (user: User) => {
-  console.log(user);
+  svc.getRepos(user.login, (repos: Repo[]) => {
+    let sortedRepos = _.sortBy(repos, [(repo: Repo) => repo.forks_count * -1])
+    user.repos = sortedRepos;
+    console.log(user);
+  })
 });
-
-svc.getRepos("hfpp2012", (repos: Repo[]) => {
-  console.log(repos);
-})
